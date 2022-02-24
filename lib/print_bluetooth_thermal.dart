9f6cf9f0ace2b9e748916fc80011e9e5d628f4fd
Andres Perez Melo <61465925+andresperezmelo@users.noreply.github.com>
2022-02-24 12:24:01 -0500

    Add files via upload

--- lib/print_bluetooth_thermal.dart
+++ lib/print_bluetooth_thermal.dart
@@ -17,6 +17,21 @@ class PrintBluetoothThermal {
     return bluetoothState;
   }*/
 
+  ///Check if it is allowed on Android 12 access to Bluetooth onwards
+  static Future<bool> get isPermissionBluetoothGranted async {
+    //bluetooth esta disponible?
+    bool bluetoothState = false;
+    try {
+      bluetoothState = await _channel.invokeMethod('ispermissionbluetoothgranted');
+      //print("llego: $bluetoothState");
+    } on PlatformException catch (e) {
+      print("Fallo Bluetooth status: '${e.message}'.");
+    }
+
+    return bluetoothState;
+  }
+
+  ///returns true if bluetooth is on
   static Future<bool> get bluetoothEnabled async {
     //bluetooth esta prendido?
     bool bluetoothState = false;
@@ -29,20 +44,28 @@ class PrintBluetoothThermal {
     return bluetoothState;
   }
 
-  static Future<List> get pairedBluetooths async {
+  ///retronates all paired bluetooth on the device
+  static Future<List<BluetoothInfo>> get pairedBluetooths async {
     //bluetooth vinculados
-    List items = [];
+    List<BluetoothInfo> items = [];
     try {
       final List result = await _channel.invokeMethod('pairedbluetooths');
       //print("llego: $result");
-      items = result;
+      await Future.forEach(result, (element) {
+        String item = element as String;
+        List<String> info = item.split("#");
+        String name = info[0];
+        String mac = info[1];
+        items.add(BluetoothInfo(name: name, macAdress: mac));
+      });
     } on PlatformException catch (e) {
-      print("Fallo Bluetooth vinculados: '${e.message}'.");
+      print("Fail pairedBluetooths: '${e.message}'.");
     }
 
     return items;
   }
 
+  //returns true if you are currently connected to the printer
   static Future<bool> get connectionStatus async {
     //estado de la conexion eon el bluetooth
     try {
@@ -55,19 +78,23 @@ class PrintBluetoothThermal {
     }
   }
 
+  ///send connection to ticket printer and wait true if it was successful, the mac address of the printer's bluetooth must be sent
   static Future<bool> connect({required String macPrinterAddress}) async {
     //conectar impresora bluetooth
-    String mac = macPrinterAddress; //"66:02:BD:06:18:7B";
     bool result = false;
+
+    String mac = macPrinterAddress; //"66:02:BD:06:18:7B";
+
     try {
       result = await _channel.invokeMethod('connect', mac);
-      print("llego conexion: $result");
+      print("result status connect: $result");
     } on PlatformException catch (e) {
       print("Failed to connect: ${e.message}");
     }
     return result;
   }
 
+  ///send bytes to print, esc_pos_utils_plus package must be used, returns true if successful
   static Future<bool> writeBytes(List<int> bytes) async {
     //enviar bytes a la impresora
     try {
@@ -80,6 +107,7 @@ class PrintBluetoothThermal {
     }
   }
 
+  ///Strings are sent to be printed by the PrintTextSize class can print from size 1 (50%) to size 5 (400%)
   static Future<bool> writeString({required PrintTextSize printText}) async {
     ///EN: you must send the enter \n to print the complete phrase, it is not sent automatically because you may want to add several
     /// horizontal values ​​of different size
@@ -100,11 +128,13 @@ class PrintBluetoothThermal {
     }
   }
 
+  ///gets the android version where it is running, returns String
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  ///get the percentage of the battery returns int
   static Future<int> get batteryLevel async {
     int result = 0;
 
@@ -117,6 +147,28 @@ class PrintBluetoothThermal {
     }
     return result;
   }
+
+  ///disconnect print
+  static Future<bool> get disconnect async {
+    bool status = false;
+    try {
+      status = await _channel.invokeMethod('disconnect');
+      //print("llego: $result");
+    } on PlatformException catch (e) {
+      print("Failed to disconnect: '${e.message}'.");
+    }
+
+    return status;
+  }
+}
+
+class BluetoothInfo {
+  late String name;
+  late String macAdress;
+  BluetoothInfo({
+    required this.name,
+    required this.macAdress,
+  });
 }
 
 class PrintTextSize {
