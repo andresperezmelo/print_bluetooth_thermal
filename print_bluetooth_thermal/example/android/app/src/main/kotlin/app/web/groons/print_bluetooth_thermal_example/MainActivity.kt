ab0532f08125cea0520be98d35589da89e9709f6
Andres Perez Melo <61465925+andresperezmelo@users.noreply.github.com>
2023-05-16 11:22:25 -0500

    Delete print_bluetooth_thermal directory

--- print_bluetooth_thermal/example/android/app/src/main/kotlin/app/web/groons/print_bluetooth_thermal_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.web.groons.print_bluetooth_thermal_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- print_bluetooth_thermal/ios/Classes/PrintBluetoothThermalPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PrintBluetoothThermalPlugin : NSObject<FlutterPlugin>
-@end
